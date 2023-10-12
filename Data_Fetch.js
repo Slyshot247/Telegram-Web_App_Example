1b1e5477b2b1a3f0e9c65569aab924400a1dcdaf
Slyshot247 <36980892+Slyshot247@users.noreply.github.com>
2023-10-12 02:06:15 -0500

    Update Data_Fetch.js

--- Data_Fetch.js
+++ Data_Fetch.js
@@ -1,7 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const map = L.map('map').setView([32.3527, -90.8779], 13); // Vicksburg coordinates
+    // ... [Your previous map initialization code]
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19
-    }).addTo(map);
+    // Adding the legend
+    const legend = L.control({ position: 'bottomright' });
+    legend.onAdd = function(map) {
+        const div = L.DomUtil.create('div', 'legend');
+        const grades = [0, 0.2, 0.4, 0.6, 0.8, 1];
+        const colors = ['#00f', '#0af', '#ff0', '#fa0', '#f00'];
+        
+        // Title for the legend
+        div.innerHTML += '<strong>Heatmap Intensity</strong><br>';
+        
+        // Generating a color square for each intensity
+        for (let i = 0; i < grades.length; i++) {
+            div.innerHTML += 
+                '<i style="background:' + colors[i] + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+        return div;
+    };
+    legend.addTo(map);
 });
