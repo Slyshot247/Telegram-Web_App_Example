f7ea34733923d46651eb81fdc425e47c3087d02d
Slyshot247 <36980892+Slyshot247@users.noreply.github.com>
2023-10-12 01:31:16 -0500

    Update Data_Fetch.js

--- Data_Fetch.js
+++ Data_Fetch.js
@@ -7,19 +7,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const heatmapLayer = new L.heatLayer([], {radius: 25}).addTo(map);
 
-    fetch('https://cors-anywhere.herokuapp.com/https://vicksburgnews.com/feed/')
-
-        .then(response => response.text())
-        .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-        .then(data => {
-            const items = data.querySelectorAll("item");
-            const heatmapData = Array.from(items).map(item => {
-                // For simplicity, generate random coords around Vicksburg
-                const lat = 32.3527 + (Math.random() - 0.5) * 0.1;
-                const lng = -90.8779 + (Math.random() - 0.5) * 0.1;
-                return [lat, lng, 0.5]; // 0.5 is the intensity; you can modify this based on some news metric if needed
-            });
-
-            heatmapLayer.setLatLngs(heatmapData);
-        });
-});
+    fetch('https://cors-anywhere.herokuapp.com/https://vicksburgnews.com/feed/', {
+        headers: {
+            "X-Requested-With": "XMLHttpRequest"
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        return response.text();
+    })
+    .then(str => new
